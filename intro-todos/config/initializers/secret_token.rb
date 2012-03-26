b66cdfdedf915ad897a794ee04b3e0a0c3eab1ca
Alex Berry <alex@apprentice.io>
2012-03-26 09:44:43 -0400

    initial commit

---
+++ intro-todos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntroTodos::Application.config.secret_token = 'b9668dd97d9a7f0bbf8e1565ff83e3d9534a3bcaf33c3a7cd633760132023df1f71e0cad13a7eab2ce64d0b8f0035a893af66bc427009a09377894af348248cb'
