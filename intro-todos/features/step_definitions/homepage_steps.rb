d1270632a2b0133cb8ada58d059fdc20b47e3ea2
Alex Berry <alex@apprentice.io>
2012-03-26 10:14:07 -0400

    added step

---
+++ intro-todos/features/step_definitions/homepage_steps.rb
@@ -0,0 +1,16 @@
+When /^I go to the homepage$/ do
+  visit "/"
+end
+
+Then /^I should see the product name in the header$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the product description$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be able to sign up for an account$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
